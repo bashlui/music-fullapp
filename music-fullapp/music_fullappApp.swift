6c8cffdef8f60633f629ef7c68a2d85599970a38
bashlui <luisboltech005@outlook.com>
2025-08-22 11:53:27 -0600

    Initial Commit

---
+++ music-fullapp/music_fullappApp.swift
@@ -0,0 +1,17 @@
+//
+//  music_fullappApp.swift
+//  music-fullapp
+//
+//  Created by toño on 22/08/25.
+//
+
+import SwiftUI
+
+@main
+struct music_fullappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
